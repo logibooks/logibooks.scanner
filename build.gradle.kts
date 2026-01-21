19e58f98acaccefa8e8ebf7ce23092d290de11d8
Maxim Samsonov <m.samsonov@computer.org>
2026-01-21 10:09:37 +0300

    fix: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,50 @@
-// Top-level build file.
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.squareup.moshi") version "1.15.1" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.logibooks.scanner"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.logibooks.scanner"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    
+    // Retrofit & Moshi for networking and JSON parsing
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.6.1" apply false
+        id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+        id("com.google.devtools.ksp")
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -13,5 +18,4 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MT93ScannerClient"
-include(":app")
+rootProject.name = "scanner"
